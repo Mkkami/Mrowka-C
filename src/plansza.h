f7b3397fde3f0f7c0f3e120214d7f79ec6b8bc0d
Michał Marciniak <michal.mar725@gmail.com>
2024-01-10 15:30:45 +0100

    Przeniesienie funkcji main oraz struktur    +main.c    +struktury.h

--- src/plansza.h
+++ src/plansza.h
@@ -4,29 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-enum Kierunek {
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST,
-};
+#include "struktury.h"
 
-enum Kolor {
-    WHITE,
-    BLACK,
-};
-
-typedef struct Mrowka {
-    int x;
-    int y;
-    enum Kierunek direction;
-} Mrowka;
-
-typedef struct Plansza {
-    int GRID_M;
-    int GRID_N;
-    enum Kolor **dane;
-} Plansza;
 
 void stworz_plansze(Plansza *plansza, int GRID_M, int GRID_N);
 
---
+++ src/struktury.h
@@ -0,0 +1,28 @@
+#ifndef _STRUCT_H
+#define _STRUCT_H
+
+enum Kierunek {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST,
+};
+
+enum Kolor {
+    WHITE,
+    BLACK,
+};
+
+typedef struct Mrowka {
+    int x;
+    int y;
+    enum Kierunek direction;
+} Mrowka;
+
+typedef struct Plansza {
+    int GRID_M;
+    int GRID_N;
+    enum Kolor **dane;
+} Plansza;
+
+#endif
